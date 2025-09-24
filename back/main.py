10d91069bfdbe9bdc567954244c07aba904ca15b
melina <mretamozo@fi.uba.ar>
2025-09-23 23:13:06 -0300

    creo carpetas separadas para el back y el front

---
+++ back/main.py
@@ -0,0 +1,18 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import users, items
+
+app = FastAPI()
+
+# Permitir que React acceda al backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # dominio de tu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users.router)
+app.include_router(items.router)
