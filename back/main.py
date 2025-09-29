c85ee30ba387fab365461ba1eb4234c2a1784e45
matias <csagastume@fi.uba.ar>
2025-09-29 16:54:24 -0300

    rebase con main

--- back/main.py
+++ back/main.py
@@ -1,11 +1,12 @@
 # main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import users, expenses
+from routers import users, expenses, auth
 from dotenv import load_dotenv
 
 # Cargar variables de entorno
 load_dotenv()
+
 
 app = FastAPI()
 
@@ -14,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",      # Create React App default
-        "http://localhost:5173",      # Vite default  
+        "http://localhost:5173",      # Vite default
         "http://127.0.0.1:3000",      # Localhost alternativo
         "http://127.0.0.1:5173",      # Vite localhost alternativo
         "http://localhost:8080",      # Otros frameworks
@@ -27,3 +28,4 @@ app.add_middleware(
 
 app.include_router(users.router)
 app.include_router(expenses.router)
+app.include_router(auth.router)
