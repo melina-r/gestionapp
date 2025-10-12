58e47d2e79a6be5ed3864ae2f735c874d1cb793b
trinidad <trinidad@talkabroad.com>
2025-10-12 15:11:12 -0300

    elimino graficos

--- front/src/pages/dashboard.jsx
+++ front/src/pages/dashboard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import BalanceCards from '../components/balance_cards';
 import Resume from '../components/resume';
-import Graphics from '../components/graphics';
 import '../styles/dashboard.css';
 
 const Dashboard = () => {
@@ -9,7 +8,6 @@ const Dashboard = () => {
         <div className="dashboard-tab">
             <BalanceCards />
             <Resume />
-            <Graphics />
         </div>
     );
 };
